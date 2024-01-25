5308422bb42f955877866525a6c08f6e6a08af73
Elias <github@ielaajez.com>
2024-01-25 13:27:46 +0100

    test: autogenerate packages

--- generate-go.sh
+++ generate-go.sh
@@ -3,4 +3,5 @@
 # the Go code is meant to be included as a git submodule
 
 # make sure you have installed the proto compiler (protoc) and dependencies. See: https://protobuf.dev/getting-started/gotutorial/
-protoc --go_out=paths=source_relative:./gen -I./definitions definitions/**/*.proto -I./definitions definitions/*.proto
+mkdir -p ./packages/go/gen
+protoc --go_out=paths=source_relative:./packages/go/gen -I./definitions definitions/**/*.proto -I./definitions definitions/*.proto
---
+++ packages/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/VU-ASE/shared-MessagingDefinitions/packages/go
+
+go 1.21.6
